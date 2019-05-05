dfd868230492ffab3f94e2ecef8ba28b02203f08
Henrique Tavares <ihenrits@gmail.com>
2019-05-05 12:55:12 -0300

    Estilizaçao

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { Container, Repository } from './style';
+
 const CompareList = () => (
-  <div>
-    <div>
+  <Container>
+    <Repository>
       <header>
         <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
         <strong>React</strong>
@@ -22,8 +24,52 @@ const CompareList = () => (
           3 days ago <small>last commit</small>
         </li>
       </ul>
-    </div>
-  </div>
+    </Repository>
+
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>React</strong>
+        <small>Facebook</small>
+      </header>
+      <ul>
+        <li>
+          95.019 <small>stars</small>
+        </li>
+        <li>
+          95.019 <small>forks</small>
+        </li>
+        <li>
+          95.019 <small>issues</small>
+        </li>
+        <li>
+          3 days ago <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
+        <strong>React</strong>
+        <small>Facebook</small>
+      </header>
+      <ul>
+        <li>
+          95.019 <small>stars</small>
+        </li>
+        <li>
+          95.019 <small>forks</small>
+        </li>
+        <li>
+          95.019 <small>issues</small>
+        </li>
+        <li>
+          3 days ago <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+  </Container>
 );
 
 export default CompareList;
