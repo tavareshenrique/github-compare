8135e8e844d766201b5d5a53a01194dd07338c1b
Henrique Tavares <ihenrits@gmail.com>
2019-05-05 13:11:05 -0300

    Enviando repositorio com Props Dados estaticos por enquanto

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -2,73 +2,31 @@ import React from 'react';
 
 import { Container, Repository } from './style';
 
-const CompareList = () => (
+const CompareList = ({ repositories }) => (
   <Container>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
-      <ul>
-        <li>
-          95.019 <small>stars</small>
-        </li>
-        <li>
-          95.019 <small>forks</small>
-        </li>
-        <li>
-          95.019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
-      <ul>
-        <li>
-          95.019 <small>stars</small>
-        </li>
-        <li>
-          95.019 <small>forks</small>
-        </li>
-        <li>
-          95.019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="facebook" />
-        <strong>React</strong>
-        <small>Facebook</small>
-      </header>
-      <ul>
-        <li>
-          95.019 <small>stars</small>
-        </li>
-        <li>
-          95.019 <small>forks</small>
-        </li>
-        <li>
-          95.019 <small>issues</small>
-        </li>
-        <li>
-          3 days ago <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+    {repositories.map(repository => (
+      <Repository>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
+        <ul>
+          <li>
+            {repository.stargazers_count} <small>stars</small>
+          </li>
+          <li>
+            {repository.forks_count} <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count} <small>issues</small>
+          </li>
+          <li>
+            {repository.pushed_at} <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
   </Container>
 );
 
